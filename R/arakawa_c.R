4916a60c6414016f53e889e8927c5595a0650b00
Michael Sumner <mdsumner@gmail.com>
2015-08-01 17:02:03 +1000

    more on grid nodes

--- R/arakawa_c.R
+++ R/arakawa_c.R
@@ -3,7 +3,7 @@ library(gris)
 library(rgl)
 library(RTriangle)
 
-f <- "D:\\data\\roms\\mer_his_1992_01.nc"
+f <- "data/mer_his_1992_01.nc"
 ulon <- raster(f, varname = "lon_u")
 ulat <- raster(f, varname = "lat_u")
 vlon <- raster(f, varname = "lon_v")
@@ -38,7 +38,7 @@ mkcvget <- function(getter) {
   }
 }
 
-ext <- extent(0, 40, 180, 201)
+ext <- extent(0, 10, 190, 201)
 
 get <- mkget(f)
 vget <- mkvget(get, ext)
@@ -48,14 +48,33 @@ colget <- mkcvget(get)
 crho <- "firebrick"
 cu <- "dodgerblue"
 cv <- "green"
-par(pch = ".")
-plot(vget("lon_u"), vget("lat_u"), xlim = range(vget("lon_u"), vget("lon_v"), vget("lon_rho")), col = cu, )
+par(pch = 16, cex = 0.5)
+plot(vget("lon_u"), vget("lat_u"), xlim = range(vget("lon_u"), vget("lon_v"), vget("lon_rho")), col = cu)
 points(vget("lon_v"), vget("lat_v"), col = cv)
 points(vget("lon_rho"), vget("lat_rho"), col = crho)
-for (i in 1:360) {
-lines(colget("lon_v", i), colget("lat_v", i))
+
+for (i in 1:36) {
+lines(colget("lon_u", i), colget("lat_u", i), col = "red")
 }
 
-for (i in 1:200) {
-  lines(rowget("lon_v", i), rowget("lat_v", i))
+for (i in 1:20) {
+  lines(rowget("lon_v", i), rowget("lat_v", i), col = "red")
 }
+
+l <- vector("list", 46)
+cnt <- 0
+for (i in 1:359) {
+  cnt <- cnt + 1
+  l[[cnt]] <- cbind(colget("lon_u", i), colget("lat_u", i))
+}
+
+for (i in 1:199) {
+  cnt <- cnt + 1
+  l[[cnt]] <- cbind(rowget("lon_v", i), rowget("lat_v", i))
+}
+
+library(sp)
+ll <- SpatialLines(list(Lines(lapply(l, Line), "1")))
+library(rgeos)
+g <- gPolygonize(gNode(ll))
+
