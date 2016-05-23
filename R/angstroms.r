88165bc2c0c0b4d3a480deb6585cd2d9a37905a3
Michael Sumner <mdsumner@gmail.com>
2016-05-23 15:27:34 +1000

    stray spfromtable

--- R/angstroms.r
+++ R/angstroms.r
@@ -51,7 +51,7 @@ romsmap.SpatialPolygonsDataFrame <- function(x, coords, crop = FALSE, lonlat = T
   index <- expand.grid(x = seq(ncol(coords)), y = rev(seq(nrow(coords))))[kd$nn.idx, ]
  tab$x_ <- index$x
   tab$y_ <- index$y
-  spbabel::spFromTable(tab, crs = NA_character_)
+  spbabel::sp(tab, crs = NA_character_)
 }
 
 #' @rdname romsmap
