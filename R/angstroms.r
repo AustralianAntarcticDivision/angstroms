6ea51eb1858851e91d8b8e97ee5b25a6ed628673
Michael Sumner <mdsumner@gmail.com>
2016-06-20 14:57:09 +1000

    transpose

--- R/angstroms.r
+++ R/angstroms.r
@@ -72,6 +72,7 @@ romsmap.SpatialPointsDataFrame <- romsmap.SpatialPolygonsDataFrame
 #' @param x ROMS file name
 #' @param spatial names of coordinate variables (e.g. lon_u, lat_u) 
 #' @param ncdf default to NetCDF no matter what file name
+#' @param transpose the extents (ROMS is FALSE, Access is TRUE)
 #' @param ... unused
 #'
 #' @return RasterStack with two layers of the 2D-variables
@@ -82,7 +83,7 @@ romsmap.SpatialPointsDataFrame <- romsmap.SpatialPolygonsDataFrame
 #'   coord <- romscoord("roms.nc")
 #' }
 #' @importFrom raster stack
-romscoords <- function(x, spatial = c("lon_u", "lat_u"), ncdf = TRUE,  ... ) {
+romscoords <- function(x, spatial = c("lon_u", "lat_u"), ncdf = TRUE,  transpose = FALSE, ... ) {
   l <- vector("list", length(spatial))
   for (i in seq_along(l)) l[[i]] <- raster(x, varname = spatial[i], ncdf = TRUE, ...)
   l <- lapply(l, function(x) setExtent(x, extent(0, nrow(x), 0, ncol(x))))
