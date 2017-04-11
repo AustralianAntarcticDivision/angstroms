5e51b8a221d56be07c8e8b7b01486e90bbb9955d
Michael Sumner <mdsumner@gmail.com>
2017-04-11 21:52:17 +1000

    read it every which way

--- R/nc-utils.r
+++ R/nc-utils.r
@@ -1,4 +1,67 @@
+raster_ispace <- function(x, transpose = TRUE) {
+  x <- t(x[,ncol(x):1])
+  if (transpose) {
+    e <- extent(0, ncol(x), 0, nrow(x)) 
+  } else {
+    e <- extent(0, nrow(x), 0, ncol(x))
+  }
+  setExtent(raster(x), e)
+}
 
+#' @examples 
+#' x <- raadtools:::cpolarfiles()$fullname[1]
+#' plot(roms_xy(x, "u"))
+#' plot(roms_xz(x, "u", slice = c(392L,1L)), asp = NA)
+#' plot(roms_xt(x, "u", slice = c(392L,1L)), asp = NA)
+#' 
+#' plot(roms_yz(x, "u"))
+#' plot(roms_yt(x, "u", slice = c(1L,1L)), asp = NA)
+#' plot(roms_zt(x, "u", slice = c(1L, 392L)), asp = NA)
+#' @name romsdata
+#' @export
+roms_xy <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(1L, 1L, slice)
+  count <- c(-1L, -1L, 1L, 1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
+#' @name romsdata
+#' @export
+roms_xz <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(1L, slice[1L], 1L, slice[2L])
+  count <- c(-1L, 1L, -1L, 1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
+#' @name romsdata
+#' @export
+roms_xt <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(1L, slice[1L], slice[2L], 1L)
+  count <- c(-1L, 1L, 1L, -1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
+
+
+#' @name romsdata
+#' @export
+roms_yz <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(slice[1L], 1L, 1L, slice[2L])
+  count <- c(1L, -1L, -1L,  1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
+#' @name romsdata
+#' @export
+roms_yt <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(slice[1L], 1L,  slice[2L], 1L)
+  count <- c(1L, -1L,  1L, -1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
+
+#' @name romsdata
+#' @export
+roms_zt <- function(x, varname, slice = c(1L, 1L), transpose = TRUE, ...) {
+  start <- c(slice, 1L, 1L)
+  count <- c(1L, 1L, -1L, -1L)
+  raster_ispace(ncgetslice(x, varname, start = start, count = count))
+}
 
 
 #' Extract a data layer from ROMS by name and slice. 
@@ -16,7 +79,7 @@
 #' @return RasterLayer
 #' @export
 #'
-romsdata <- function (x, varname, slice = c(1, 1), ncdf = TRUE, transpose = FALSE, ...) 
+romsdata <- function (x, varname, slice = c(1L, 1L), ncdf = TRUE, transpose = FALSE, ...) 
 {
    stopifnot(!missing(varname))
   if (is.null(x)) stop("x must be a valid file name")
@@ -53,7 +116,7 @@ ncget <- function(x, varname) {
   ncdf4::ncvar_get(nc, varname)
 }
 
-ncgetslice <- function(x, varname, start, count) {
+ncgetslice <- function(x, varname, start = c(1L, 1L, 1L, 1L), count = c(-1L, -1L, -1L, -1L)) {
   con <- ncdf4::nc_open(x)
   on.exit(ncdf4::nc_close(con))
   ncdf4::ncvar_get(con, varname, start = start, count = count)
@@ -63,9 +126,9 @@ ncgetslice <- function(x, varname, start, count) {
 rastergetslice <- function(x, slice) {
   ## expect slice to be c(xindex, NA, NA) or c(NA, yindex, NA)
   ## all longitudes
-  if (is.na(slice[1]))  x1 <-  setExtent(raster(getValuesBlock(x, row = slice[2], nrows = 1)), extent(0, ncol(x), 0, nlayers(x)))
+  if (is.na(slice[1]))  x1 <-  setExtent(raster(getValuesBlock(x, row = slice[2], nrows = 1L)), extent(0, ncol(x), 0, nlayers(x)))
   ## all latitudes
-  if (is.na(slice[2]))  x1 <-  setExtent(raster(getValuesBlock(x, col = slice[1], ncols = 1, nrows = nrow(x))), extent(0, nrow(x), 0, nlayers(x)))
+  if (is.na(slice[2]))  x1 <-  setExtent(raster(getValuesBlock(x, col = slice[1], ncols = 1L, nrows = nrow(x))), extent(0, nrow(x), 0, nlayers(x)))
   x1
 }
 
