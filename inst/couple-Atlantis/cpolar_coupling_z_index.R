83640479cacda4869c76f99155607ba5485c0d9d
Michael Sumner <mdsumner@gmail.com>
2017-04-21 15:14:03 +1000

    apply to all time slices

--- inst/couple-Atlantis/cpolar_coupling_z_index.R
+++ inst/couple-Atlantis/cpolar_coupling_z_index.R
@@ -8,6 +8,14 @@
 
 ##corners <- read.delim("https://raw.githubusercontent.com/cecilieha/NoBA/master/corners_neighbours_nordic.txt")
 
+## curvlinear vector rotation: https://www.myroms.org/forum/viewtopic.php?f=3&t=295
+
+## hydro stuff
+# https://confluence.csiro.au/display/Atlantis/Forcing+Files
+# https://confluence.csiro.au/display/Atlantis/Current+Forcing+File+Structure
+# https://confluence.csiro.au/display/Atlantis/HydroConstruct
+# https://confluence.csiro.au/display/Atlantis/Matlab+-+Hydro
+
 ## dummy hydro
 # sohydro <- "~/projects/AtlantisEastAntartica/SOhydrodummy.nc"
 # library(ncdump)
@@ -34,6 +42,16 @@ library(raadtools)
 library(ncdump)
 ## get the Circumpolar files
 cpolar <- raadtools:::cpolarfiles()
+
+## this single dataframe records every time slice across
+## all files in a row, so we can loop over time step below
+file_db <- bind_rows(lapply(cpolar$fullname, function(x) {
+  nc <- NetCDF(x)
+  tlen <- filter(nc$dimension, name == "ocean_time")$len
+  tibble(fullname = rep(x, tlen), band_level = seq_len(tlen))
+}))
+
+
 roms_file <- cpolar$fullname[2]
 
 ## get a BGM and read it
@@ -46,6 +64,8 @@ roms_ll <- romscoords(roms_file, transpose = TRUE)
 
 ## we need the unsullied boxes to identify points inside them
 boxes <- boxSpatial(bgm)
+roms_face <- romsmap(project_to(faceSpatial(bgm), "+init=epsg:4326"), roms_ll)
+
 ## which box does each point fall in
 index_box <- function(box_sp, roms_ll) {
   ind <- sp::over(project_to(coords_points(roms_ll), box_sp) , as(box_sp, "SpatialPolygons"))
@@ -59,7 +79,11 @@ index_box <- function(box_sp, roms_ll) {
 ## and each face for the ROMS cells it traverses
 library(dplyr)
 box_roms_index <- index_box(boxes, roms_ll)
-  
+ind_face <- cellFromLine(romsdata(roms_file, "u"), roms_face)
+face_roms_index <- tibble(face = rep(seq_len(nrow(roms_face)), lengths(ind_face)), 
+                          cell = unlist(ind_face))
+
+
 #' return the ramp of positive depths from surface down 
 #' (so that the order is native to the NetCDF order)
 roms_level <- function(Cs_r, h, cell) {
@@ -69,29 +93,47 @@ roms_level <- function(Cs_r, h, cell) {
 set_indextent <- function(x) {
   setExtent(x, extent(0, ncol(x), 0, nrow(x)))
 }
-h <- raster(roms_file, varname = "h")
+## important to readAll here, else extract is very slow in the loop
+h <- readAll(raster(roms_file, varname = "h"))
 ## Cs_r is the S-coord stretching
 Cs_r <- rawdata(roms_file, "Cs_r")
 
 
 ## build the level index between Atlantis and ROMS
-box_roms_index$botz <- boxes$botz[box_roms_index$box]
+
 list_nc_z_index <- vector('list', nrow(box_roms_index))
+max_depth <- max(extract(h, unique(box_roms_index$cell)))
+atlantis_depths <- cumsum(c(0, rev(rbgm::build_dz(-max_depth))))
 for (i in seq_len(nrow(box_roms_index))) {
   rl <- roms_level(Cs_r, h, box_roms_index$cell[i])
   ## implicit 0 at the surface, and implicit bottom based on ROMS
-  list_nc_z_index[[i]] <- findInterval(rl, cumsum(c(0, rev(rbgm::build_dz(box_roms_index$botz[i])))))
+  list_nc_z_index[[i]] <- findInterval(rl, atlantis_depths)
 if (i %% 1000 == 0) print(i)
-  }
-
+}
 ## join the box-xy-index to the level index
-z_index <- bind_rows(lapply(list_nc_z_index, 
+box_z_index <- bind_rows(lapply(list_nc_z_index, 
                  function(x) tibble(atlantis_level = x, roms_level = seq_along(x))), 
           .id = "cell_index") %>% 
   inner_join(mutate(box_roms_index, cell_index = as.character(row_number()))) %>% 
-  select(-cell_index, -botz)
+  select(-cell_index)
 
-#saveRDS(z_index, file = "z_index.rds")
+for (i in seq_len(nrow(face_roms_index))) {
+  rl <- roms_level(Cs_r, h, face_roms_index$cell[i])
+  ## implicit 0 at the surface, and implicit bottom based on ROMS
+  list_nc_z_index[[i]] <- findInterval(rl, atlantis_depths)
+  if (i %% 1000 == 0) print(i)
+}
+## join the face-xy-index to the level index
+face_z_index <- bind_rows(lapply(list_nc_z_index, 
+                                function(x) tibble(atlantis_level = x, roms_level = seq_along(x))), 
+                         .id = "cell_index") %>% 
+  inner_join(mutate(face_roms_index, cell_index = as.character(row_number()))) %>% 
+  select(-cell_index)
+
+
+
+#z_index$atlantis_depth <- c(0, rev(rbgm::build_dz(box_roms_index$botz[i])))[z_index$atlantis_level]
+#z_index$roms_depth <- rl[z_index$roms_level]
 ## driver function to loop over x raster by levels
 ## matching $cell for the right group
 extract_at_level <- function(x, cell_level) {
@@ -105,11 +147,28 @@ extract_at_level <- function(x, cell_level) {
   values
 }
 
-ru <- set_indextent(brick(roms_file, varname = "u", lvar = 4, level = 1))
-z_index$u <- extract_at_level(ru, rename(z_index, level = roms_level, cell = cell))
+box_props <- face_props <- vector("list", nrow(file_db))
+for (i_timeslice in seq(nrow(file_db))) {
+  roms_file <- file_db$fullname[i_timeslice]
+  level <- file_db$band_level[i_timeslice]
+  ru <- set_indextent(brick(roms_file, varname = "u", lvar = 4, level = level))
+  face_z_index$u <- extract_at_level(ru, rename(face_z_index, level = roms_level, cell = cell))
+  rv <- set_indextent(brick(roms_file, varname = "v", lvar = 4, level = level))
+  face_z_index$v <- extract_at_level(rv, rename(face_z_index, level = roms_level, cell = cell))
+  rdata <- set_indextent(brick(roms_file, varname = "temp", lvar = 4, level = level))
+  box_z_index$temp <- extract_at_level(rdata, rename(box_z_index, level = roms_level, cell = cell))
+  rdata <- set_indextent(brick(roms_file, varname = "salt", lvar = 4, level = level))
+  box_z_index$salt <- extract_at_level(rdata, rename(box_z_index, level = roms_level, cell = cell))
+  box_props[[i_timeslice]] <- box_z_index %>% group_by(atlantis_level, box) %>% 
+    summarize(temp = mean(temp, na.rm = TRUE), salt = mean(salt ,na.rm = TRUE)) %>% 
+    mutate(band_level = level)
+  face_props[[i_timeslice]] <-  face_z_index %>% group_by(atlantis_level, face) %>% 
+    summarize(flux = mean(sqrt(u * u + v * v), na.rm = TRUE) * 24 * 3600) %>% 
+    mutate(band_level = level)
+}
 
-rv <- set_indextent(brick(roms_file, varname = "v", lvar = 4, level = 1))
-z_index$v <- extract_at_level(rv, rename(z_index, level = roms_level, cell = cell))
+
+
 
 
 plot(romsmap(boxes[15, ], roms_ll), add = F, border = "red", lwd = 4)
@@ -126,11 +185,6 @@ ggplot(mutate(z_index %>%
 rm(ru, rv)
 
 
-rdata <- set_indextent(brick(roms_file, varname = "temp", lvar = 4, level = 1))
-z_index$temp <- extract_at_level(rdata, rename(z_index, level = roms_level, cell = cell))
-
-rdata <- set_indextent(brick(roms_file, varname = "salt", lvar = 4, level = 1))
-z_index$salt <- extract_at_level(rdata, rename(z_index, level = roms_level, cell = cell))
 ggplot(z_index %>% mutate(x = xFromCell(rdata, cell), 
                           y = yFromCell(rdata, cell)), 
        aes(x = x, y = y, fill = temp)) + geom_tile() + 
