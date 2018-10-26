bef651f0e3e8709a851de7298cf3f3d31dbf9188
Michael Sumner <mdsumner@gmail.com>
2018-10-26 06:46:29 +0000

    structure for box and face properties

--- R/atlantis_hydro.R
+++ R/atlantis_hydro.R
@@ -23,6 +23,25 @@ transport_dimensions <- function(bgm, ntime = 365 * 2, nlevels = 2L,
   list(var.time, var.face, var.lev, var.destb, var.srcb, var.pt1x, var.pt2x, var.pt1y, var.pt2y, var.trans)
 }
 
+mass_dimensions <- function(bgm, ntime = 365 * 2, nlevels = 2L, 
+                            time_unit = "seconds since 1970-01-01 00:00:00") {
+  timedim   <- ncdim_def("time", "", seq_len(ntime), unlim = TRUE, create_dimvar = FALSE)
+  leveldim  <- ncdim_def("level", "", seq_len(nlevels), create_dimvar = FALSE)
+  boxesdim  <- ncdim_def("boxes", "", seq_len(bgm$extra$nbox), create_dimvar = FALSE)
+  
+  var.time <- ncvar_def("time", time_unit, timedim, prec="double")
+  var.box  <- ncvar_def("boxes", "", boxesdim, longname="Box IDs", prec='integer')
+  var.lev  <- ncvar_def("level", "", leveldim, longname="layer index; 1=near surface; positive=down" ,
+                        prec="integer")
+  var.vertflux <- ncvar_def("verticalflux", "m3/s", list(leveldim, boxesdim, timedim), -999,
+                            longname = "vertical flux averaged over floor of box", prec = "float")
+  var.temp     <- ncvar_def("temperature", "degree_C", list(leveldim, boxesdim, timedim), -999,
+                            longname = "temperature volume averaged", prec = "float")
+  var.salt     <- ncvar_def("salinity", "psu", list(leveldim,boxesdim,timedim), -999,
+                            longname = "salinity volume averaged", prec = "float")
+  
+  list(var.time,var.box, var.lev, var.salt, var.temp, var.vertflux)
+}
 
 #' Create "transport" file for Atlantis. 
 #'
@@ -100,9 +119,58 @@ create_transport <- function(filename,
   }
   v1 <- bgm$facesXverts[bgm$facesXverts$.p0 == 1, ] %>%  dplyr::inner_join(ll, ".vx0")
   v2 <- bgm$facesXverts[bgm$facesXverts$.p0 == 2, ] %>%  dplyr::inner_join(ll, ".vx0")
+  #browser()
   ncvar_put(nc_transp, "pt1_x",  v1$x)
   ncvar_put(nc_transp, "pt2_x",  v1$y)
   ncvar_put(nc_transp, "pt1_y",  v2$x)
   ncvar_put(nc_transp, "pt2_y",  v2$y)
+  filename
+}
+
+
+create_mass <- function(filename,
+                             model_title = "Box averaged properties file [placeholder]", 
+                             bgmfilepath = "",
+                             bgmlevels = NULL, 
+                             time_steps = NULL,
+                             mass_params = "", overwrite = FALSE) {
+  
+  
+  bgm <- rbgm::bgmfile(bgmfilepath)
+  
+  if (is.null(bgmlevels)) {
+    deltas <- rbgm::build_dz(as.numeric(bgm$extra$maxwcbotz))
+    bgmlevels <- cumsum(c(bgm$extra$maxwcbotz, deltas))
+    message(sprintf("'bgmlevels' not supplied, building levels at %s", paste(bgmlevels, collapse = ", ")))
+    
+  } 
+  nlevels <- length(bgmlevels)
+  if (is.null(time_steps)) {
+    time_steps <- seq(as.POSIXct("1970-01-01 00:00:00", tz = "UTC"), by = 12 * 3600, length = 10)
+  }
+  
+  stopifnot(inherits(time_steps, "POSIXct"))
+  stopifnot(length(unique(diff(unclass(time_steps)))) == 1)
+  
+  dimensions <- mass_dimensions(bgm, nlevels = nlevels)
+  
+  if (file.exists(filename) && !overwrite) {
+    stop(sprintf("'filename' already exists, use 'overwrite = TRUE' or delete: \n%s", filename))
+  }
+  nc_varfile <-  ncdf4::nc_create(filename, dimensions)
+  on.exit(ncdf4::nc_close(nc_varfile), add = TRUE)
+  
+
+  #assign global attributes to file
+  ncatt_put(nc_varfile,0,"title", model_title)
+  ncatt_put(nc_varfile, 0, "geometry", bgmfilepath)
+  ncatt_put(nc_varfile, 0, "parameters", mass_params)
+
+  #assign attributes to variables
+  ncatt_put(nc_varfile, "time","dt",86400,prec="double")
+
+  ncvar_put(nc_varfile, "time", time_steps, count = length(time_steps))
+  ncvar_put(nc_varfile, "level", bgmlevels)
+  ncvar_put(nc_varfile, "boxes", bgm$boxes$.bx0)
   filename
 }
--- inst/couple-Atlantis/ncdf_templates.R
+++ inst/couple-Atlantis/ncdf_templates.R
@@ -4,68 +4,31 @@
 library(ncdf4)
 
 ### FOR TRANSPORT NC FILE
-filename = sprintf("%s_transport.nc", tempfile())
-
-model_title <- "Transport file [placeholder]"
-
+transp_filename <- sprintf("%s_transport.nc", tempfile())
+mass_filename  <- sprintf("%s_mass.nc", tempfile())
 transp_params <- ""
-
-
-
+mass_params <- ""
 
 bgmfilepath <- bgmfiles::bgmfiles("antarctica_28")
-
-
-nc_transp <- create_transport(filename, model_title = "Transport file Antarctica_28", bgmfilepath = bgmfilepath, 
+library(angstroms)
+create_transport(transp_filename, model_title = "Transport file Antarctica_28", bgmfilepath = bgmfilepath, 
                               bgmlevels = 1:9, time_steps = Sys.time() + (1:10) * 24 * 3600)
 
+create_mass(mass_filename, model_title = "Mass file Antarctica_28", bgmfilepath = bgmfilepath)
 
+nctransp <- ncdf4::nc_open(transp_filename, write = TRUE)
+transport <- array(rnorm(9 * 90 * 10), c(9, 90, 10))
+ncvar_put(nctransp, "transport", transport, count = dim(transport))
+nc_close(nctransp)
 
-transport <- array(0, c(9, 90, 10))
-## count:  
-## If not specified and the variable does NOT have an unlimited dimension, the entire variable is written. 
-## If the variable has an unlimited dimension, this argument must be specified.
-ncvar_put(nc_transp, "transport", transport, count = dim(transport))
-ncvar_get(nc_transp, "time")
+ncmass <- ncdf4::nc_open(mass_filename, write = TRUE)
 
-nc_close(nc_transp)
-
-
-### For T, S, Vertical Flux NC file
-filename="RM_NEUS_variables_2010_20180325_fix.nc"
-
-#define dimensions
-timedim=ncdim_def("time", "", 1:length(t_tot), unlim=T, create_dimvar = F) #as.double(t_tot)
-leveldim=ncdim_def("level", "", 1:nlevel, create_dimvar = F)
-boxesdim=ncdim_def("boxes", "", 1:nboxes, create_dimvar = F)
-
-#create variables
-#NB!!!!!! Unlimited rec needs to be on the right - otherwise R complains!
-#origMissVal_ex=0.0
-var.time=ncvar_def("time","seconds since 1964-01-01 00:00:00 +10",timedim,prec="double")
-var.box=ncvar_def("boxes", "", boxesdim, longname="Box IDs", prec='integer')
-var.lev=ncvar_def("level","",leveldim,longname="layer index; 1=near surface; positice=down" ,prec="integer")
-var.vertflux=ncvar_def("verticalflux","m3/s",list(leveldim, boxesdim, timedim),-999,longname="vertical flux averaged over floor of box",prec="float")
-var.temp=ncvar_def("temperature","degree_C",list(leveldim, boxesdim, timedim),-999,longname="temperature volume averaged",prec="float")
-var.salt=ncvar_def("salinity","psu",list(leveldim,boxesdim,timedim),-999,longname="salinity volume averaged",prec="float")
-
-nc_varfile=nc_create(filename,list(var.time,var.box, var.lev, var.salt, var.temp, var.vertflux))
-
-#assign global attributes to file
-ncatt_put(nc_varfile,0,"title","Box averaged properties file, NEUS")
-ncatt_put(nc_varfile,0,"geometry","neus_tmerc_RM.bgm")
-ncatt_put(nc_varfile,0,"parameters","")
-
-#assign attributes to variables
-ncatt_put(nc_varfile,var.time,"dt",86400,prec="double")
+salinity <- temperature <- vertical_flux <- array(rnorm(9 * 28 * 10), c(9, 28, 10))
+dm <- dim(vertical_flux)
 
 #assign variables to file
-ncvar_put(nc_varfile,var.vertflux,vertical_flux, count=c(nlevel,nboxes, ntimes))
-ncvar_put(nc_varfile,var.time,t_tot)
-ncvar_put(nc_varfile,var.lev,atl.level)
-ncvar_put(nc_varfile,var.salt,salinity, count=c(nlevel,nboxes, ntimes))
-ncvar_put(nc_varfile,var.temp,temperature, count=c(nlevel,nboxes, ntimes))
-ncvar_put(nc_varfile,var.box,box.boxes)
-
-nc_close(nc_varfile)
+ncvar_put(ncmass, "verticalflux", vertical_flux, count = dm)
+ncvar_put(ncmass, "salinity", salinity, count = dm)
+ncvar_put(ncmass, "temperature",temperature, count= dm)
+nc_close(ncmass)
 
