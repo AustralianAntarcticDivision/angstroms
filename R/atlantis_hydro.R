5459f14ee54925c11224f5c81635a846f82f005e
Michael Sumner <mdsumner@gmail.com>
2019-01-23 21:41:10 +1100

    fix #16 (swapped x/y)

--- R/atlantis_hydro.R
+++ R/atlantis_hydro.R
@@ -120,10 +120,10 @@ create_transport <- function(filename,
   v1 <- bgm$facesXverts[bgm$facesXverts[[".p0"]] == 1, ] %>%  dplyr::inner_join(ll, ".vx0")
   v2 <- bgm$facesXverts[bgm$facesXverts[[".p0"]] == 2, ] %>%  dplyr::inner_join(ll, ".vx0")
   
-  #browser()
+
   ncvar_put(nc_transp, "pt1_x",  v1$x)
-  ncvar_put(nc_transp, "pt2_x",  v1$y)
-  ncvar_put(nc_transp, "pt1_y",  v2$x)
+  ncvar_put(nc_transp, "pt2_x",  v2$x)
+  ncvar_put(nc_transp, "pt1_y",  v1$y)
   ncvar_put(nc_transp, "pt2_y",  v2$y)
   filename
 }
